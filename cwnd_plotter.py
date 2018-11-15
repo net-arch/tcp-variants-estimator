1c6a633972948d065dcf09c7af28444537baa62d
Naoki Ohzeki <0h23k1.n40k1@gmail.com>
2018-11-15 22:36:17 +0900

    feat: adapt df source

--- cwnd_plotter.py
+++ cwnd_plotter.py
@@ -1,15 +1,21 @@
-import os, sys
+import os
+import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
 def main():
-    filepath = sys.argv[1]
-    df = pd.read_csv(filepath)
+    input = sys.argv[1]
+
+    df = pd.read_csv(input)
     df = df[['ts', 'cwnd']]
     df.plot(x=df.columns[0])
-    # plt.save('images/{}.png')
-    plt.show()
+
+    if len(sys.argv) > 2:
+        output = sys.argv[2]
+        plt.save(output)
+    else:
+        plt.show()
 
 
 if __name__ == '__main__':
