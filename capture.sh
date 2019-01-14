3d4378eb1621013b2cc0edf2e60299e3d74ed627
Naoki Ohzeki <0h23k1.n40k1@gmail.com>
2019-01-14 19:29:33 +0900

    feat: add capture.sh

---
+++ capture.sh
@@ -0,0 +1,22 @@
+ALGO_LIST="reno cubic bbr bic westwood htcp hybla vegas scalable lp veno yeah illinois"
+
+for ALGO in $ALGO_LIST; do
+  for i in {0..9}; do
+    FILEPATH="data/dump/${ALGO}.${i}.dump"
+    rm -f $FILEPATH
+
+    echo
+    echo [ $ALGO $i ]
+    echo
+
+    sleep 2
+    tcpdump host 192.168.1.2 -i enp0s25 -w $FILEPATH &
+    PID=$!
+    echo capture started... PID:$PID
+
+    sleep 2
+    iperf3 -c 192.168.1.2 -t 600 -C ${ALGO}
+    kill ${PID}
+    echo capture stopped... PID:$PID
+  done
+done
